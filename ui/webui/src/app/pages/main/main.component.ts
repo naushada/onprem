931a05ed3e1e3d991a7203bb1b61af91284c7d68
naushada <naushad.dln@gmail.com>
2023-08-20 00:02:47 +0530

    Updated

--- ui/webui/src/app/pages/main/main.component.ts
+++ ui/webui/src/app/pages/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs';
+import { EventService } from 'src/common/event.service';
 
 @Component({
   selector: 'app-main',
@@ -8,8 +10,18 @@ import { Component } from '@angular/core';
 export class MainComponent {
 
   selectedItem:string = "Config";
-  constructor() {
-
+  role:string = "basic";
+  constructor(private event: EventService) {
+    event.subscribe("user.login", (id:String, document:string) => {
+      if(id == "user.login") {
+        let response = JSON.parse(JSON.stringify(document));
+        if(response["status"] == "success") {
+          this.role = response["role"];
+        } else {
+          this.role = "basic";
+        }
+      }
+    });
   }
 
   onClick(item:string) {
