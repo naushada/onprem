5673a317f4b69015d7c158b089b0f83df056e375
naushada <naushad.dln@gmail.com>
2023-08-19 20:41:54 +0530

    Updated

--- ui/webui/src/app/app.component.ts
+++ ui/webui/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'webui';
+export class AppComponent implements OnInit {
+  title = 'onprem';
+
+  constructor(private rt: Router) {}
+
+  ngOnInit(): void {
+    this.rt.navigateByUrl('/login');
+  }
 }
--- ui/webui/src/app/pages/login/login.component.ts
+++ ui/webui/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm: FormGroup
+  constructor(private fb: FormBuilder, private rt:Router) {
+    this.loginForm = this.fb.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  get username() {
+    return this.loginForm.value.username;
+  }
+
+  get password() {
+    return this.loginForm.value.password;
+  }
+
+  onLogin() {
+
+    this.rt.navigateByUrl('/main');
+
+    if(!this.username.length || !this.password.length) {
+      alert("User ID or Password Can't be Empty");
+    }
+    /*
+    this.http.getlogininfo(this.username, this.password).subscribe((accountinfo:IAccountInfo) => {
+      let id:string = "user.login";
+      let document: string = JSON.stringify(accountinfo);
+      this.event.publish({id, document});
+      this.rt.navigateByUrl('/main');
+    },
+    (error) => {
+      alert("Invalid Credentials");
+    },
+    () => {
+
+    });
+    */
+  }
 }
